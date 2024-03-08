9ef2b6dd7427f63661e2384fdfe6c0aa087578dd
Ben Rowe <ben.rowe.83@gmail.com>
2024-03-08 13:59:46 +1100

    Create rector.php

---
+++ rector.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Php71\Rector\FuncCall\CountOnNullRector;
+use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths(
+        [
+            __DIR__ . '/app',
+        ]
+    );
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets(
+        [
+            LevelSetList::UP_TO_PHP_82,
+            PHPUnitSetList::PHPUNIT_100,
+        ]
+    );
+    $rectorConfig->skip(
+        [
+            ReadOnlyPropertyRector::class,
+            CountOnNullRector::class,
+            NullToStrictStringFuncCallArgRector::class,
+            AddLiteralSeparatorToNumberRector::class,
+        ]
+    );
+};
